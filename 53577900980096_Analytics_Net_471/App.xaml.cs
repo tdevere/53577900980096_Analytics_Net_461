5f26315f454dcd2759cc1e2dca4777d7cea9b94d
tdevere <tdevere@microsoft.com>
2022-03-24 19:44:24 -0500

    Add project files.

---
+++ 53577900980096_Analytics_Net_471/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+
+
+namespace _53577900980096_Analytics_Net_471
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start("d11d553d-509d-478b-ae75-162d0e7f5d65",
+                   typeof(Analytics));
+            Analytics.TrackEvent("OnStartup");
+            base.OnStartup(e);
+        }
+    }
+}
